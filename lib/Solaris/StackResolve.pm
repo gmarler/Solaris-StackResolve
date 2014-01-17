bf989530f3050858b1d842c3f65f1ec6e512334a
Gordon Marler <gmarler@gmarler.com>
2014-01-17 15:02:21 -0500

    Add some POD

--- lib/Solaris/StackResolve.pm
+++ lib/Solaris/StackResolve.pm
@@ -4,4 +4,10 @@ use warnings;
 package Solaris::StackResolve;
 # ABSTRACT: Resolves user stacks for huge binaries on Solaris, gathered by DTrace
 
+=head1
+
+This module works to resolve user stacks collected by DTrace.
+
+=cut
+
 1;
