4225737f2eeae7ae26df94a682c6ed82948fa630
Gordon Marler <gmarler@gmarler.com>
2014-01-17 15:12:59 -0500

    Use standard version

--- lib/Solaris/StackResolve.pm
+++ lib/Solaris/StackResolve.pm
@@ -3,6 +3,7 @@ use warnings;
 
 package Solaris::StackResolve;
 # ABSTRACT: Resolves user stacks for huge binaries on Solaris, gathered by DTrace
+use version 0.77; our $VERSION = version->declare("v0.0.1");
 
 =head1
 
