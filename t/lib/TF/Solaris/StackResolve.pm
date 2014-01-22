1034743f29a145b555384a0507b20488b99508e3
Gordon Marler <gmarler@gmarler.com>
2014-01-22 01:49:31 -0500

    First tests for Solaris::StackResolve

---
+++ t/lib/TF/Solaris/StackResolve.pm
@@ -0,0 +1,46 @@
+#
+# TF means "TestsFor"
+#
+package TF::Solaris::StackResolve;
+
+use Test::Class::Moose;
+with 'Test::Class::Moose::Role::AutoUse';
+
+sub test_startup {
+  my ($test) = shift;
+
+  if ( ! -x q{/bin/uname} or ($^O ne "solaris")) {
+    $test->test_skip("These tests only valid on Solaris");
+  }
+
+  $test->next::method;
+
+  # Log::Log4perl Configuration in a string ...
+  my $conf = q(
+    #log4perl.rootLogger          = DEBUG, Logfile, Screen
+    log4perl.rootLogger          = DEBUG, Screen
+
+    #log4perl.appender.Logfile          = Log::Log4perl::Appender::File
+    #log4perl.appender.Logfile.filename = test.log
+    #log4perl.appender.Logfile.layout   = Log::Log4perl::Layout::PatternLayout
+    #log4perl.appender.Logfile.layout.ConversionPattern = [%r] %F %L %m%n
+
+    log4perl.appender.Screen         = Log::Log4perl::Appender::Screen
+    log4perl.appender.Screen.stderr  = 0
+    log4perl.appender.Screen.layout = Log::Log4perl::Layout::SimpleLayout
+  );
+
+  # ... passed as a reference to init()
+  Log::Log4perl::init( \$conf );
+}
+
+sub test_constructor {
+  my ($test) = shift;
+
+  my $obj = $test->class_name->new(pid => $$);
+
+  isa_ok($obj, $test->class_name, 'Should create new object');
+}
+
+
+1;
